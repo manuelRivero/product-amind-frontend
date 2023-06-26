e74af94ee9c8d989e467bd16e6e6259506e05bf1
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-06-26 16:44:12 -0300

    [add] render backend url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: `http://localhost:5000/`,
+    baseURL: `https://product-admin-backend.onrender.com`,
 })
 export default client;
