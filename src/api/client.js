7ee15d8f3c6c9cc65bb01df79f7bfc7e260887ad
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2025-01-02 21:28:51 -0300

    axios instance credentials true

--- src/api/client.js
+++ src/api/client.js
@@ -3,5 +3,6 @@ import axios from "axios";
 const client = axios.create({
     // baseURL: `https://product-admin-backend.onrender.com`,
     baseURL: `http://localhost:5000`,
+    withCredentials: true,
 })
 export default client;
