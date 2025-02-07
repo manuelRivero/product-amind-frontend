1c5f5d77400f54f5f25b10aa9807fee0b0359be5
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2025-02-06 22:46:59 -0300

    [fix] loading state on product detail

--- src/views/AddProducts/index.js
+++ src/views/AddProducts/index.js
@@ -333,7 +333,7 @@ export default function AddProducts() {
         }
     }, [productDetail])
 
-    if (loadingProductDetail || loadingCategoriesData) {
+    if ((params.id && loadingProductDetail) || loadingCategoriesData) {
         return <LoadinScreen />
     }
     console.log('values ', watch())
