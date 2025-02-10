38ed5eafa40e4e53359f63462c56a9502d4c189d
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2025-02-10 15:21:25 -0300

    [fix] use env on client

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const client = axios.create({
     //baseURL: `https://product-admin-backend.onrender.com`,
     // baseURL: `https://08e0-2803-9800-98c4-7f3d-8053-24eb-db73-bb88.ngrok-free.app`,
-     baseURL: `http://localhost:5000`,
+     baseURL: process.env.REACT_APP_API_KEY,
     withCredentials: true,
 })
 export default client
